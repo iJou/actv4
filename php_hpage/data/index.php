56f984bc810d8ba11f7412c9cb0c60ba025a4543
iJou <aikoncap.std@gmail.com>
2021-03-27 12:00:33 -0700

    php-homepage

---
+++ php_hpage/data/index.php
@@ -0,0 +1,36 @@
+﻿<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=1, initial-scale=1.0">
+    <title>ProdTec Co.</title>
+    <link rel="stylesheet" href="styleindex.css">
+</head>
+
+<body>
+    <header>
+        <div class="contentheader">
+            <dir></dir>
+            <nav>
+                <ul class="menu">
+                    <li><a href="index.html">Inicio</a></li>
+                </ul>
+            </nav>
+
+
+        </div>
+    </header>
+
+    <?php include 'home.php';?>
+    
+    <footer>
+        <div class="conteninfo">
+            <h1>ProdTec Co.</h1>
+            <p>Página diseñada por ProdTec Co.| Todos los derechos reservados</p>
+            
+        </div>
+    </footer>
+</body>
+
+</html>
